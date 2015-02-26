a2f5ce13c9fb3acd1da3af92b4642cae74db8b43
Ben Olson <molson5@vols.utk.edu>
2015-02-26 18:11:45 -0500

    Fixed Xlib support, fast refresh now

--- bmp.h
+++ bmp.h
@@ -6,7 +6,7 @@ void print_24b_array(unsigned char *pixmap, int *width, int *height)
   for(int i = 0; i < (*height); ++i) {
     for(int n = 0; n < (*width); ++n) {
       for(int x = 0; x < 4; ++x) {
-        printf(" %3.5x ", *pixmap);
+        printf(" %3.2x ", *pixmap);
         ++pixmap;
       }
       printf("\n");
@@ -94,20 +94,42 @@ void read_pixels(FILE *file, unsigned char *pixmap, int *width, int *height, int
   // Make sure we can get back to the beginning
   unsigned char *beg = pixmap;
 
+  // Put the pointer at the end of the array (since BMPs are stored backwards)
+  for(int i = 0; i < ((*height) * (*width) * 4); ++i) {
+    ++pixmap;
+  }
+/*
   if(*depth == 24) {
     unsigned char *beg = pixmap;
     for(int i = 0; i < (*height); ++i) {
+      for(int n = 0; n < (*width); ++n) {
+        *pixmap = 0;
+        pixmap -= 3;
+        for(int x = 0; x < 3; ++x) {
+          fread(pixmap, 1, 1, file);
+          ++pixmap;
+        }
+        pixmap -= 4;
+      }
+      fseek(file, padding, SEEK_CUR);
+    }
+  }
+  */
+  if(*depth == 24) {
+    unsigned char *beg = pixmap;
+    for(int i = 0; i < (*height); ++i) {
+      pixmap -= ((*width) * 4);
       for(int n = 0; n < (*width); ++n) {
         for(int x = 0; x < 3; ++x) {
           fread(pixmap, 1, 1, file);
           ++pixmap;
         }
-        *pixmap = 1;
+        *pixmap = 0;
         ++pixmap;
       }
       fseek(file, padding, SEEK_CUR);
+      pixmap -= ((*width) * 4);
     }
   }
-
   pixmap = beg;
 }
