75bd79cbbdfee31e2f01f4884708499f13a2d187
Ben Olson <molson5@utk.edu>
2015-04-06 17:25:08 +0000

    Added support for testing on the Jetson

--- graph/graph.r
+++ graph/graph.r
@@ -1,14 +1,19 @@
 library("ggplot2")
 
-frames60 <- read.csv("frames60.csv")
-frames1200 <- read.csv("frames1200.csv")
+wash_frames60 <- read.csv("wash_frames60.csv")
+wash_frames1200 <- read.csv("wash_frames1200.csv")
+jetson_frames60 <- read.csv("jetson_frames60.csv")
 
-png("frames60.png", width=1600, height=900, res=300)
-ggplot(frames60, aes(x=numthreads, y=time, col=exec)) +
+png("wash_frames60.png", width=1600, height=900, res=300)
+ggplot(wash_frames60, aes(x=numthreads, y=time, col=exec)) +
   geom_line()
 
-png("frames1200.png", width=1600, height=900, res=300)
-ggplot(frames1200, aes(x=numthreads, y=time, col=exec)) +
+png("wash_frames1200.png", width=1600, height=900, res=300)
+ggplot(wash_frames1200, aes(x=numthreads, y=time, col=exec)) +
+  geom_line()
+
+png("jetson_frames60.png", width=1600, height=900, res=300)
+ggplot(jetson_frames60, aes(x=numthreads, y=time, col=exec)) +
   geom_line()
 
 dev.off()
--- testing.pl
+++ testing.pl
@@ -4,11 +4,12 @@ open(my $fh, ">", "data.csv");
 print $fh "\"exec\",\"numthreads\",\"time\"\n";
 my @numthreads = (
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  20, 30, 40, 50, 100);
+  20, 30, 40, 50, 100, 300);
 my @execs = (
   "bimage_nothread",
   "bimage_thread",
-  "bimage_threadpool");
+  "bimage_threadpool",
+  "bimage_cuda");
 my @images = (
   "images/flowers.bmp" );
 
@@ -21,7 +22,7 @@ foreach my $exec (@execs) {
   foreach my $image (@images) {
     foreach my $numthread (@numthreads) {
       my $output;
-      if($exec eq "bimage_nothread") {
+      if(($exec eq "bimage_nothread") or ($exec eq "bimage_cuda")) {
         $output = `./$exec $image`;
       } else {
         $output = `./$exec $image $numthread`;
